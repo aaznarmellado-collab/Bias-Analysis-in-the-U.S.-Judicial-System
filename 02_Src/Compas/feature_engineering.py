a6e98b353aeefd3b1a13c7b06de69d1a4b07170d
AlbertoAnsar <a.aznarmellado@gmail.com>
2026-04-11 18:11:43 +0200

    Revert "Third notebook final version"
    This reverts commit 84f6b11b52e088d2c05bb0733e0273549d536e43.

--- 02_Src/Compas/feature_engineering.py
+++ 02_Src/Compas/feature_engineering.py
@@ -18,10 +18,3 @@ def one_hot_encoding(df, column, drop_val):
     )
 
     return df_encoded
-
-
-def compute_weight(df, row):
-    P_A = df["race"].value_counts(normalize=True)
-    P_Y = df["two_year_recid"].value_counts(normalize=True)
-    P_AY = df.groupby(["race","two_year_recid"]).size() / len(df)
-    return (P_A[row["race"]] * P_Y[row["two_year_recid"]]) / P_AY[row["race"], row["two_year_recid"]]
